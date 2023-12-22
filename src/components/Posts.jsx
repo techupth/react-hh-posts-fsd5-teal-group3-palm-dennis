47d761cab01077bfeaa24103a6cce78267c651f7
Palm <palml1lax@gmail.com>
2023-12-22 07:38:01 +0700

    feat: finish ex

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,62 @@
+import { useState } from "react";
+import userData from "./data/userData";
+
 function Posts() {
+  const [postData, setPostData] = useState([...userData]);
+  const [likeCount, setLikeCount] = useState(
+    postData.map((item) => {
+      return item.likes;
+    })
+  );
+  const addLike = (index) => {
+    const newCount = likeCount[index] + 1;
+    likeCount.splice(index, 1, newCount);
+    setLikeCount([...likeCount]);
+  };
+
+  const disLike = (index) => {
+    if (likeCount[index] > 0) {
+      const newCount = likeCount[index] - 1;
+      likeCount.splice(index, 1, newCount);
+      setLikeCount([...likeCount]);
+    }
+  };
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      <div className="post-list">
+        {postData.map((post, i) => {
+          return (
+            <div className="post-item" key={i}>
+              <div className="post-header">
+                <h2>{post.title}</h2>
+                <div className="post-social-media-stats">
+                  <span className="stats-topic">Likes:</span>
+                  <span className="post-likes">{likeCount[i]}</span>
+                </div>
+              </div>
+              <p className="post-content">{post.content}</p>
+              <div className="post-actions">
+                <button
+                  className="like-button"
+                  onClick={() => {
+                    addLike(i);
+                  }}
+                >
+                  Like
+                </button>
+                <button
+                  className="dislike-button"
+                  onClick={() => {
+                    disLike(i);
+                  }}
+                >
+                  Dislike
+                </button>
+              </div>
             </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
